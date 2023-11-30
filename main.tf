584e713ddbb4040ed59d8c5d763dc9a9edc6bf75
vbonl78 <152090575+vbonl78@users.noreply.github.com>
2023-12-01 03:19:26 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "public_instance" {
  ami           = var.ami
  instance_type = var.instance_type
- subnet_id = subnet-0a5420a45589263a9
+ subnet_id = "subnet-0a5420a45589263a9"
 
  tags = {
    Name = var.name_tag,
